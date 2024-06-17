d50816f414e34972f446888744b1032c948df50d
Carissa <carissamoodley6@gmail.com>
2024-06-17 15:06:24 +0200

    docstrings added for main/models.py

--- main/models.py
+++ main/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Book_read(models.Model):
-    '''This model sets up a database for Books read with fields for the 
-    books title, author, date read and an image of the books cover.'''
+    '''
+    This model represents a book read in the book club.
+    '''
 
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -12,26 +13,38 @@ class Book_read(models.Model):
     image = models.ImageField(upload_to='book_images/', blank=True, null = True)
 
     def __str__(self):
+        '''
+        String representation of the Book_read model.
+        '''
         return self.title
 
 class Book_next(models.Model):
-    '''This model sets up a database for books to be read next with 
-    fields for the books title, author and an image for the books cover.'''
+    '''
+    This model represents a book that could be read next month by
+    the book club.
+    '''
 
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     image = models.ImageField(upload_to='book_images/', blank=True, null = True)
 
     def __str__(self):
+        '''
+        String representation of the Book_next model.
+        '''
         return self.title
 
 class Vote(models.Model):
-    '''This model sets up a database for the user to vote for a book to
-    read for next month with fields for the user, book and date voted.'''
+    '''
+    This model represents a vote for a book to be read next by user.
+    '''
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book_next, on_delete=models.CASCADE)
     vote_date = models.DateField(auto_now_add = True)
 
     def __str__(self):
+        '''
+        String representation of the Vote model.
+        '''
         return f"{self.user.username} voted for {self.book.title}"
